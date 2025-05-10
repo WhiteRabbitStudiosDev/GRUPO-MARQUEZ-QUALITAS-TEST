5cd41b067bb2f7836498f5608d69e30e17c935e8
White Rabbit Studios <153143079+WhiteRabbitStudiosDev@users.noreply.github.com>
2025-05-10 04:28:07 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,21 @@
-color = ['#f9a363','#8c1346','#d13d3c','#e2663b'];
-function addHeart(){
-  randomColor = Math.floor(Math.random() * (4));
-  randomX = Math.floor(Math.random() * (100));
-  randomY = Math.floor(Math.random() * (100));
-  svg = '<svg style="top:'+randomY+'%; left: '+randomX+'%;" class="heart" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 23.217 23.217" style="enable-background:new 0 0 23.217 23.217;" xml:space="preserve"><g><path style="fill:'+color[randomColor]+';" d="M11.608,21.997c-22.647-12.354-6.268-27.713,0-17.369C17.877-5.716,34.257,9.643,11.608,21.997z" /></g></svg>';
-  $("body").append(svg);
-}
-function deleteHearts(){
-  $('.heart:lt(100)').remove();
-}
-function centerHeart(){
-  $(".heart").css('-webkit-animation','show-heart 4s ease');
-}
-setInterval(addHeart,50);
-setInterval(centerHeart,200);
-setInterval(deleteHearts,8000);
+$(document).ready(function(){
+ 
+
+  $('.valentines-day').click(function(){
+      // Animación de desvanecimiento de los elementos del sobre
+      $('.envelope').css({'animation':'fall 3s linear 1', '-webkit-animation':'fall 3s linear 1'});
+      $('.envelope').fadeOut(800, function() {
+          // Ocultar elementos dentro de .valentines-day
+          $('.valentines-day .heart, .valentines-day .text, .valentines-day .front').hide();
+
+          // Hacer visible la carta con una animación ondulante
+          $('#card').css({'visibility':'visible', 'opacity': 0, 'transform': 'scale(0.1)'});
+          $('#card').animate({'opacity': 1}, {duration: 1000, step: function(now, fx) {
+              var scale = 1 + Math.sin(now * Math.PI) * 0.1; // Calculamos la escala basada en la función seno
+              $(this).css('transform', 'scale(' + scale + ')');
+          }}); // Animación de ondulación
+      });
+  });
+});
+
+ 
